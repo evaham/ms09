132aee4ae3d3de0ac950d5c5e4411c7be220d8bf
JungWoo Ham <evaham@naver.com>
2025-03-28 16:45:03 +0900

    페이지 구조 추가 및 레이아웃 수정

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,10 +19,17 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div>
+          <div className="flex h-16 border">
+            <Link className="mr-4" href="/">첫화면</Link>
+            <a>레이어팝업<i>아이콘</i></a>
+            <div className="ml-auto">신청조회</div>
+          </div>
+        </div>
         {children}
       </body>
     </html>

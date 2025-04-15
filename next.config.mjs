64d6441ae59620b65d5462063c29b4dd6c666578
JungWoo Ham <evaham@naver.com>
2025-04-15 09:52:53 +0900

    Add GitHub Pages configuration to next.config.mjs; set output, basePath, assetPrefix, and disable image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+// 환경변수로 GitHub Pages 여부 판단
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+
+const nextConfig = {
+    output: 'export', // 반드시 static export 설정
+    basePath: isGithubPages ? '/ms09' : '',
+    assetPrefix: isGithubPages ? '/ms09/' : '',
+    images: {
+      unoptimized: true, // next/image 최적화 비활성화 (필수!)
+    }
+};
 
 export default nextConfig;
